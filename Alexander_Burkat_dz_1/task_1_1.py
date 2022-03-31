409c2d3bd8c79d498bd975d865b46f0b2f6d1813
uheif <applenoise@gmail.com>
2022-03-31 03:05:51 +0300

    task_1_1 done

---
+++ Alexander_Burkat_dz_1/task_1_1.py
@@ -0,0 +1,21 @@
+# Реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах:
+# до минуты: <s> сек;
+# до часа: <m> мин <s> сек;
+# до суток: <h> час <m> мин <s> сек;
+# * в остальных случаях: <d> дн <h> час <m> мин <s> сек.
+
+duration = 3600
+
+secs = duration % 60
+mins = duration // 60 % 60
+hours = duration // 60 ** 2 % 24
+days = duration // 60 ** 2 // 24
+
+if duration < 60:
+    print(duration, 'сек')
+elif days:
+    print(days, 'дн', hours, 'час', mins, 'мин', secs, 'сек')  # 4 дн 15 час 9 мин 13 сек
+elif hours:
+    print(hours, 'час', mins, 'мин', secs, 'сек')
+else:
+    print(mins, 'мин', secs, 'сек')
