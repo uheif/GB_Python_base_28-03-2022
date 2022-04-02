5cc3760de5364c72aed198a71bbb25b9f5c3cf04
uheif <applenoise@gmail.com>
2022-04-02 16:41:26 +0300

    task_2_2 done

--- Alexander_Burkat_dz_2/task_2_2.py
+++ Alexander_Burkat_dz_2/task_2_2.py
@@ -1,16 +1,24 @@
 weather = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
-for el in weather:
-    if el.lstrip('+-').isdigit() and weather[weather.index(el) - 1] != '"' and weather[weather.index(el) + 1] != '"':
-        weather.insert(weather.index(el) + 1, '"')
-        weather.insert(weather.index(el), '"')
-        if el.startswith('+') or el.startswith('-'):
-            weather[weather.index(el)] = f'{el[0]}{int(weather[weather.index(el)]):02d}'
+i = 0
+msg = ''
+
+while i < len(weather):
+    if weather[i].lstrip('+-').isdigit():
+        if weather[i].startswith('+') or weather[i].startswith('-'):
+            weather[i] = f'{weather[i][0]}{int(weather[i]):02d}'
         else:
-            weather[weather.index(el)] = f'{int(weather[weather.index(el)]):02d}'
-    elif weather[weather.index(el)] == '"' and weather[weather.index(el) - 1] != ' ':  # and weather[weather.index(el) + 1] != ' ':
-        weather.insert(weather.index(el), ' ')
+            weather[i] = f'{int(weather[i]):02d}'
+        weather.insert(i, '"')
+        weather.insert(i + 2, '"')
+        msg += ''.join(weather[i:i + 2])
+        i += 2
+    else:
+        msg += weather[i] + ' '
+        i += 1
 
 print(weather)
-print(''.join(weather))
+# ['в', '"', '05', '"', 'часов', '"', '17', '"', 'минут', 'температура', 'воздуха', 'была', '"', '+05', '"', 'градусов']
+print(msg)
+# в "05" часов "17" минут температура воздуха была "+05" градусов
 
