7071d9099bb1aeb7df3e4be16392c0d22f72de2a
uheif <applenoise@gmail.com>
2022-04-02 03:25:57 +0300

    task_2_2 add

---
+++ Alexander_Burkat_dz_2/task_2_1.py
@@ -0,0 +1,8 @@
+# Выяснить тип результата выражений:
+# ● 15 * 3
+# ● 15 / 3
+# ● 15 // 2
+# ● 15 ** 2
+
+print(type(15 * 3), type(15 / 3), type(15 // 2), type(15 ** 2))
+# <class 'int'> <class 'float'> <class 'int'> <class 'int'>
