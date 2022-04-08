4bf99a64e5685001f1ba2f7f7f4cf97871c34e5f
Alexander Burkat <applenoise@gmail.com>
2022-04-08 14:21:48 +0300

    task_3_5

--- Alexander_Burkat_dz_3/task_3_5.py
+++ Alexander_Burkat_dz_3/task_3_5.py
@@ -8,7 +8,7 @@
 # Сможете ли вы добавить еще один аргумент — флаг, разрешающий или запрещающий повторы слов в шутках
 # (когда каждое слово можно использовать только в одной шутке)? Сможете ли вы сделать аргументы именованными?
 
-from random import randrange
+from random import randrange, choice
 
 nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
@@ -21,18 +21,25 @@ def get_jokes(lst1, lst2, lst3, n=3):
         return lst[randrange(len(lst))]
 
     jokes = []
-    idxs = []
+    idxs1 = [i for i in range(len(lst1))]
+    print(idxs1)
+    idxs2 = [i for i in range(len(lst2))]
+    idxs3 = [i for i in range(len(lst3))]
 
     for i in range(n):
-        jokes.append(f'{idx(lst1)} {idx(lst2)} {idx(lst3)}')
+        idx1 = idxs1.pop(choice(idxs1))
+        print(idx1, idxs1)
+        idx2 = idxs2.pop(choice(idxs2))
+        idx3 = idxs3.pop(choice(idxs3))
+        jokes.append(f'{lst1[idx1]} {lst2[idx2]} {lst3[idx3]}')
+
         print(jokes)
-        print(idx(lst1) in jokes[i])
+        # print(idx(lst1) in jokes[i])
         # lst1_f = filter(idx(lst1) in jokes[n], lst1)
         # print(lst1_f)
 
     return jokes
 
-print('a' in 'a b')
 
 
 print(get_jokes(nouns, adjectives, lst3=adverbs))
