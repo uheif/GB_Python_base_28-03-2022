c0126927e2e4c03b1e8b9b390b9cc23cd6dcb4e4
uheif <applenoise@gmail.com>
2022-04-25 17:46:40 +0300

    task_7_3 done

--- Alexander_Burkat_dz_7/task_7_3.py
+++ Alexander_Burkat_dz_7/task_7_3.py
@@ -12,3 +12,29 @@
 # Примечание: исходные файлы необходимо оставить; обратите внимание, что html-файлы расположены в родительских папках
 # (они играют роль пространств имён); предусмотреть возможные исключительные ситуации;
 # это реальная задача, которая решена, например, во фреймворке django.
+
+import os
+import shutil
+
+
+to_copy = 'templates'
+main_root = os.path.join('my_project')
+temp = os.path.join(main_root, to_copy)
+try:
+    os.mkdir(temp)
+except FileExistsError as e:
+    print(e)
+
+for root, dirs, files in os.walk(main_root):
+    tree = os.path.join(root)
+    if to_copy in dirs:
+        full_to_copy = os.path.join(tree, to_copy)
+        for obj in os.listdir(full_to_copy):
+            obj_path = os.path.join(full_to_copy, obj)
+            try:
+                if os.path.isdir(obj_path):
+                    shutil.copytree(obj_path, os.path.join(temp, obj))
+                elif os.path.isfile(obj_path):
+                    shutil.copy2(obj_path, temp)
+            except FileExistsError as e:
+                print(e)
