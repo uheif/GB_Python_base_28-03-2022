cb429b0f2cfe647074473dba7ce5093cf998f577
uheif <applenoise@gmail.com>
2022-05-02 01:05:45 +0300

    task_9_3 done

---
+++ Alexander_Burkat_dz_9/task_9_3.py
@@ -0,0 +1,32 @@
+# Реализовать базовый класс Worker (работник).
+# определить атрибуты: name, surname, position (должность), income (доход);
+# последний атрибут должен быть защищённым и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus};
+# создать класс Position (должность) на базе класса Worker;
+# в классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+# и дохода с учётом премии (get_total_income);
+# проверить работу примера на реальных данных:
+# создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров.
+
+class Worker:
+
+    def __init__(self, name, surname, position, income: dict):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = income
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+
+w1 = Position('Ivan', 'Ivanov', 'director', {'wage': 100500, 'bonus': 100500})
+print(w1.get_full_name())
+print(w1.get_total_income())
+print(w1.name, w1.surname, w1.position, w1._income)
